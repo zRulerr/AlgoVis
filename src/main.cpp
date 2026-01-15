bf2cd5f61410e64f5ba41ae312921e4c28c2858b
Mert Sultan <merto0209@gmail.com>
2026-01-15 22:24:22 +0100

    Changes made to the Rednerer, main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,22 @@
 #include <vector>
 #include <iostream>
 
+const int screenWidth = 1280;
+const int screenHeight = 720;
+const int sidePanelWidth = 250;
+
+Rectangle uiPanel = { 0, 0, sidePanelWidth, screenHeight };
+Rectangle gridArea = { sidePanelWidth, 0, screenWidth - (2 * sidePanelWidth), screenHeight };
+Rectangle analyticsPanel = { screenWidth - sidePanelWidth, 0, sidePanelWidth, screenHeight };
+
 int main() {
     // C++17
     std::vector<int> test_version = {1, 2, 3};
     std::cout << "C++ Standard Check: Vektor-Groesse ist " << test_version.size() << std::endl;
 
     //Window init
-    InitWindow(800, 600, "AlgoVis - Pathfinder v1.0");
+    InitWindow(screenWidth, screenHeight, "AlgoVis - Pathfinder v1.0");
 
-    // Nach InitWindow(...)
     Image icon = LoadImage("resources/logo.png");
     SetWindowIcon(icon);
     UnloadImage(icon); //
@@ -23,6 +30,16 @@ int main() {
         BeginDrawing();
             ClearBackground(RAYWHITE);
         EndDrawing();
+
+        //User Interface Panel (Left)
+        DrawRectangleRec(uiPanel, LIGHTGRAY);
+        DrawLine(sidePanelWidth, 0, sidePanelWidth, screenHeight, DARKGRAY); // Trennlinie
+        DrawText("STEUERUNG", 20, 20, 20, DARKGRAY);
+
+        //Analytics Panel (Right)
+        DrawRectangleRec(analyticsPanel, ColorAlpha(LIGHTGRAY, 0.5f)); 
+        DrawLine(analyticsPanel.x, 0, analyticsPanel.x, screenHeight, DARKGRAY); // Trennlinie
+        DrawText("ANALYTICS", analyticsPanel.x + 20, 20, 20, DARKGRAY);
     }
 
     CloseWindow();
