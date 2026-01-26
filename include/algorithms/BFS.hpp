83c34e656a260899d0c20d0f9f46dfd1f0d5d2d4
Mert Sultan <merto0209@gmail.com>
2026-01-26 23:24:41 +0100

    BFS Class added

---
+++ include/algorithms/BFS.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+#include "Grid.hpp"
+
+class BFS {
+private:
+    Grid& m_grid;
+    std::vector<int> m_visitedOrder;
+
+    std::queue<int> m_frontier;
+    std::unordered_map<int, int> m_cameFrom;
+    bool m_found = false;
+
+    auto reconstructPath(const std::unordered_map<int, int>& cameFrom, int start, int target) -> std::vector<int>;
+public:
+    explicit BFS(Grid& grid);
+
+    auto BFS::initPath(int startIndex, int endIndex) -> void;
+
+    const std::vector<int>& getVisitedOrder() const {return m_visitedOrder;}
+
+    auto step(int targetIndex) -> void; 
+    bool isFinished() const { return m_found || m_frontier.empty(); }
+};
---
+++ src/algorithms/BFS.cpp
@@ -0,0 +1,52 @@
+#include "BFS.hpp"
+
+BFS::BFS(Grid& grid) : m_grid(grid) {}
+
+auto BFS::initPath(int startIndex, int endIndex) -> void{
+    //reset
+    m_frontier = std::queue<int>(); //empty queue
+    m_cameFrom.clear();
+    m_visitedOrder.clear();
+    m_found = false;
+    
+    //
+    if (!m_grid.isWalkable(startIndex) || !m_grid.isWalkable(endIndex)) return;
+
+    //
+    m_frontier.push(startIndex);
+    m_cameFrom[startIndex] = startIndex;
+}
+
+auto BFS::step(int targetIndex) -> void {
+    if (m_frontier.empty() || m_found) return;
+
+    int current = m_frontier.front();
+    m_frontier.pop();
+
+    if (current == targetIndex) {
+        m_found = true;
+        return;
+    }
+
+    for (int d = 0; d < 4; ++d) {
+        int next = m_grid.getNeighborIndex(current, static_cast<Direction>(d));
+        if (next != -1 && m_grid.isWalkable(next) && m_cameFrom.find(next) == m_cameFrom.end()) {
+            m_frontier.push(next);
+            m_cameFrom[next] = current;
+            m_visitedOrder.push_back(next);
+        }
+    }
+}
+
+auto BFS::reconstructPath(const std::unordered_map<int, int>& cameFrom, int start, int target) -> std::vector<int> {
+    std::vector<int> path;
+    int current = target;
+    
+    while (current != start) {
+        path.push_back(current);
+        current = cameFrom.at(current);
+    }
+    path.push_back(start);
+    std::reverse(path.begin(), path.end());
+    return path;
+}
