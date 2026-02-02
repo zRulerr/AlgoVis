bb6b16ddb8fa230a22159db550fc50030cb6c020
Mert Sultan <merto0209@gmail.com>
2026-02-02 12:23:20 +0100

    Some Documentation added

--- include/AppState.hpp
+++ include/AppState.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
+/** @brief Enum for the different Paint modes used when drawing Walls */
 enum class PaintMode { NONE, PAINTING, ERASING };
 
+/** @brief Struct for Special points such as Start- and Endpoint  */
 struct SpecialPoint {   
     int index;
     Color color;
 };
 
+/** @brief Struct to store changing Variables throughout Programm runtime */
 struct AppState {
     PaintMode currentPaintMode = PaintMode::NONE;
 
