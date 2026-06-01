a69fab13d87c0230c84f7a68903a26195aea97ab
Mert <108607807+zRulerr@users.noreply.github.com>
2026-06-01 10:55:16 +0200

    Refactor comments in GridTests.cpp for consistency
    Removed spaces in comments for consistency.

--- tests/GridTests.cpp
+++ tests/GridTests.cpp
@@ -2,19 +2,19 @@
 #include "core/Grid.hpp"
 
 TEST_CASE("Grid Index Check", "[Grid]") {
-    // 10 x 10 test grid
+    //10x10 test grid
     Grid testGrid(10, 10); 
 
     SECTION("Valid indices should return true") {
-        // Test the middle and the edges
-        CHECK(testGrid.isNotOutOfGrid(0));   // Top left
-        CHECK(testGrid.isNotOutOfGrid(99));  // Down right
-        CHECK(testGrid.isNotOutOfGrid(50));  // middle
+        //Test the middle and the edges
+        CHECK(testGrid.isNotOutOfGrid(0));   //Top left
+        CHECK(testGrid.isNotOutOfGrid(99));  //Down right
+        CHECK(testGrid.isNotOutOfGrid(50));  //middle
     }
 
     SECTION("Invalid indices should return false") {
-        CHECK_FALSE(testGrid.isNotOutOfGrid(-1));  // too small
-        CHECK_FALSE(testGrid.isNotOutOfGrid(100)); // too big by 1
-        CHECK_FALSE(testGrid.isNotOutOfGrid(500)); // way too big
+        CHECK_FALSE(testGrid.isNotOutOfGrid(-1));  //too small
+        CHECK_FALSE(testGrid.isNotOutOfGrid(100)); //too big by 1
+        CHECK_FALSE(testGrid.isNotOutOfGrid(500)); //way too big
     }
 }
